be9a4b8b16b6a5c028406d1aff3ce9b5d9e6321d
mahendramahara <mahendramahara369@gmail.com>
2023-08-17 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ Laboratory-Programs/Lab-1/ConditionalDemo.java
@@ -0,0 +1,47 @@
+public class ConditionalDemo {
+    public static void main(String[] args) {
+        int num = 10;
+
+        // If-else statement
+        if (num > 0) {
+            System.out.println("Number is positive");
+        } else if (num < 0) {
+            System.out.println("Number is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+
+        // Switch statement
+        int day = 3;
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+
+        System.out.println("Day of the week: " + dayName);
+    }
+}
