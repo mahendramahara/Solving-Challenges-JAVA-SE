aa32f0904848360fefa5a3d6bfe28b31483ed8c6
mahendramahara <mahendramahara369@gmail.com>
2023-09-13 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ Laboratory-Programs/Lab-4/ThreadUsingRunnableInterface.java
@@ -0,0 +1,37 @@
+public class ThreadUsingRunnableInterface {
+
+    public static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + ": Count " + i);
+                try {
+                    Thread.sleep(100); // Sleep for 100 milliseconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MyRunnable runnable = new MyRunnable();
+
+        Thread thread1 = new Thread(runnable, "Thread 1");
+        Thread thread2 = new Thread(runnable, "Thread 2");
+
+        thread1.start();
+        thread2.start();
+
+        // Wait for threads to complete
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main thread exiting.");
+    }
+}
