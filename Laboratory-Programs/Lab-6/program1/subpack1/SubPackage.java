d30dbdd7133aa4878fb959ada6be68f0b833c52a
mahendramahara <mahendramahara369@gmail.com>
2023-09-30 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ Laboratory-Programs/Lab-6/program1/subpack1/SubPackage.java
@@ -0,0 +1,11 @@
+package program1.subpack1;
+
+public class SubPackage {
+    public int a = 10;
+    public int b = 20;
+
+    public void add() {
+        int sum = a + b;
+        System.out.println("The sum of a and b is: " + sum);
+    }
+}
