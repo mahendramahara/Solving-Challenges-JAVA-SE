0dbdd5e545d2ed8fa70a1e2a9547600457ee9ea3
mahendramahara <mahendramahara369@gmail.com>
2023-10-23 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ JAVA-SE-Programs/PackageExample/Main.java
@@ -0,0 +1,15 @@
+// Title: Package Demonstration in Java
+import vehicles.Car;
+import vehicles.Motorcycle;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.drive();
+
+        Motorcycle motorcycle = new Motorcycle();
+        motorcycle.ride();
+    }
+}
+
+// © Mahendra Mahara 2023
