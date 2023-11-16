89cf637f81f716d184152a0ac1b57b95058bf6df
mahendramahara <mahendramahara369@gmail.com>
2023-11-16 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ JAVA-SE-Programs/AbstractionExample1.java
@@ -0,0 +1,29 @@
+// Title: Abstraction in Java
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+public class AbstractionExample1 {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        circle.draw();
+
+        Shape rectangle = new Rectangle();
+        rectangle.draw();
+    }
+}
+
+// © Mahendra Mahara 2023
