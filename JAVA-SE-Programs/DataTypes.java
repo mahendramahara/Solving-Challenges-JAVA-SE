7ae08496ae0dfd07573fb9a471aa4d3d28f0281a
mahendramahara <mahendramahara369@gmail.com>
2023-11-30 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ JAVA-SE-Programs/DataTypes.java
@@ -0,0 +1,28 @@
+//Java program to demonstrate Data Types Avalilable in Java
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte byteVariable = 127;
+        short shortVariable = 32767;
+        int intVariable = 2147483647;
+        long longVariable = 9223372036854775807L;
+        float floatVariable = 3.14f;
+        double doubleVariable = 3.141592653589793;
+        boolean booleanVariable = true;
+        char charVariable = 'A';
+        
+        System.out.println("Byte Variable: " + byteVariable);
+        System.out.println("Short Variable: " + shortVariable);
+        System.out.println("Int Variable: " + intVariable);
+        System.out.println("Long Variable: " + longVariable);
+        System.out.println("Float Variable: " + floatVariable);
+        System.out.println("Double Variable: " + doubleVariable);
+        System.out.println("Boolean Variable: " + booleanVariable);
+        System.out.println("Char Variable: " + charVariable);
+        
+        String stringVariable = "Hello, World!";
+        System.out.println("String Variable: " + stringVariable);
+    }
+}
+
+// Copyright © 2023 - Mahendra Mahara
