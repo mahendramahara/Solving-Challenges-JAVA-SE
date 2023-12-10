3e202f40c1d00954af2130505cbeee49a4dac9ff
mahendramahara <mahendramahara369@gmail.com>
2023-12-10 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ JAVA-SE-Programs/AppletDemo.java
@@ -0,0 +1,29 @@
+// Title: Demonstration of Java Applets
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class AppletDemo extends Applet {
+    public void init() {
+        // Initialization
+    }
+
+    public void start() {
+        // Start
+    }
+
+    public void paint(Graphics g) {
+        // Drawing
+        g.drawString("Hello, World!", 20, 20);
+    }
+
+    public void stop() {
+        // Stop
+    }
+
+    public void destroy() {
+        // Destruction
+    }
+}
+
+// © Mahendra Mahara 2023
