c588c8b9390128c015beda4c1254d05726641cd6
mahendramahara <mahendramahara369@gmail.com>
2024-01-23 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ JAVA-SE-Programs/HelloWorld.java
@@ -0,0 +1,9 @@
+// First Java Program: Print Hello World
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// Copyright © Mahendra Mahara 2023
