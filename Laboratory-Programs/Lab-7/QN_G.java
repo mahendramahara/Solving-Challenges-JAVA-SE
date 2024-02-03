c49df30bde56918b0995b9ec08fb06d4ccfa50b3
mahendramahara <mahendramahara369@gmail.com>
2024-02-03 07:00:00 +0545

    100+ Java-SE Programs With Clean Code ♨️

---
+++ Laboratory-Programs/Lab-7/QN_G.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class QN_G extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("Hello, this is a simple Java applet!", 20, 20);
+    }
+}
